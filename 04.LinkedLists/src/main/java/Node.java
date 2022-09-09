a874bde6ffc217792af9338abb04f3a77e9ef688
lgaid <104643045+lgaid@users.noreply.github.com>
2022-09-09 13:05:54 -0400

    LinkedList

---
+++ 04.LinkedLists/src/main/java/Node.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Node {
+    int id;
+    Node next;
+
+    public Node(int id) {
+        this.id = id;
+    }
+}
---
+++ 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class SinglyLinkedListApp {
+    public static void main(String[] args) {
+        MySinglyLinkedList mylist=new MySinglyLinkedList();
+        System.out.println(mylist.isEmpty());
+
+    }
+}
