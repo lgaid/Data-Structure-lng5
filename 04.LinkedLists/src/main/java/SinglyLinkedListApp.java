81d2f3c0faa4128370e91437788163bab0205cbd
lgaid <104643045+lgaid@users.noreply.github.com>
2022-09-17 18:42:36 -0400

    Linked List

--- 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ 04.LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -1,16 +1,26 @@
 package main.java;
 
-import java.util.ArrayList;
-
 public class SinglyLinkedListApp {
     public static void main(String[] args) {
+
+
         MySinglyLinkedList mylist=new MySinglyLinkedList();
-        System.out.println(mylist.isEmpty());
 
         for (int i = 0; i < 10; i++) {
             mylist.add(i);
         }
         mylist.printNodes();
-        ArrayList<Integer> blist = new ArrayList<>();
+        System.out.println("kth item from the last : "+ mylist.getKthItemFromLast(3));
+        mylist.removeKthItemFromLast(1);
+        mylist.printNodes();
+
+
+
+
     }
+
+
+
+
+
 }
